a0f80ceea593eb94f6bae7ee2bee3d43a6624efd
Zeynep Karadağ <96845897+zzynpkrdgg@users.noreply.github.com>
2025-05-14 17:56:34 +0300

    dt1

---
+++ Assets/Scripts/MyLinkedList.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class MyLinkedList<T>
+{
+    private Node<T> head;
+    public int count;
+    public MyLinkedList()
+    {
+
+        head = null;
+        count = 0;
+    }
+    public void Add(T data)
+    {
+        Node<T> newNode = new Node<T>(data);
+        if (head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node<T> current = head;
+            while (current.next != null)
+            {
+                current = current.next;
+                current.next = newNode;
+
+            }
+            count++;
+        }
+        
+    }
+    public T GetAt(int index) { 
+    
+        if (index < 0 || index >= count)
+            throw new System.IndexOutOfRangeException();
+        
+            Node <T>   current = head;
+            for ( int i = 0; i < index; i++)
+            {
+                current = current.next;
+            }
+            
+        return current.data;
+    }
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index >= count)
+            throw new System.IndexOutOfRangeException();
+
+        if (index == 0)
+        {
+            head = head.next;
+        }
+        else
+        {
+            Node<T> current = head;
+            for (int i = 0; i < index - 1; i++)
+                current = current.next;
+
+            current.next = current.next.next;
+        }
+        count--;
+    }
+
+    public void Print()
+    {
+        Node<T> current = head;
+        while (current != null)
+        {
+            UnityEngine.Debug.Log(current.data);
+            current = current.next;
+        }
+    }
+}
---
+++ Assets/Scripts/MyNode.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Node<T>
+{
+    public T data;
+    public Node<T> next;
+    public Node (T data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
