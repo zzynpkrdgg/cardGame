4d6f800d6abcf931d23c219a83c4d95855cbbd72
ttolga-a <tolga9507@gmail.com>
2025-05-14 17:17:25 +0300

    Scriptable Object eklendi

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,8 +1,11 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
+    public CardScriptableObject cardSO;
+
     public int currentHealth;
     public int AttackPower;
     public int manaCost;
@@ -10,14 +13,33 @@ public class Card : MonoBehaviour
     public TMP_Text healthText;
     public TMP_Text attackText;
     public TMP_Text manaText;
+    public TMP_Text nameText;
+    public TMP_Text actionDescriptionText;
+    public TMP_Text loreText;
+
+    public Image characterArt;
+
 
     private void Start()
     {
+        SetupCard();
+    }
+
+    private void SetupCard()
+    {
+        currentHealth = cardSO.currentHealth;
+        AttackPower = cardSO.attackPower;
+        manaCost = cardSO.manaCost;
+
+
         healthText.text = currentHealth.ToString();
         attackText.text = AttackPower.ToString();
         manaText.text = manaCost.ToString();
 
+        nameText.text = cardSO.cardName;
+        actionDescriptionText.text = cardSO.actionDescription;
+        loreText.text = cardSO.cardLore;
+
+        characterArt.sprite = cardSO.character;
     }
-
-
 }
---
+++ Assets/Scripts/CardScriptableObject.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card", order = 1)]
+public class CardScriptableObject : ScriptableObject
+{
+    public string cardName;
+
+    [TextArea]
+    public string actionDescription, cardLore;
+
+    public int currentHealth, attackPower, manaCost;
+    public Sprite character;
+}
