6eea30a57925726bf5ac470f353de28f12f39602
Zeynep Karadağ <96845897+zzynpkrdgg@users.noreply.github.com>
2025-05-14 09:45:54 +0300

    first commit
    "commit"

---
+++ Assets/Scripts/CardPoints.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+
+{
+    //public Card activeCard;
+    public bool isPlayerPoint;
+   
+}
---
+++ Assets/Scripts/CardPointss.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+public class CardPointss : MonoBehaviour
+
+{
+    //public Card activeCard;
+    public bool isPlayerPoint;
+
+}
